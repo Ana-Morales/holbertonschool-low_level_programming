e9d3d5a24348884b3970a1b151d9455fcbedbd37
Ana-Morales <moralesanaruth@gmail.com>
2020-06-10 22:51:24 +0000

    Using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This script prints a sentence
+ * Return: 0
+ */
+int main(void)
+{
+	pritnf("with proper grammar, but the outcome is a piece of art\n");
+	return (0);
+}
