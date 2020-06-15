97b167720d58175a6e9f875adbe3db05cec47b68
Ana-Morales <moralesanaruth@gmail.com>
2020-06-15 22:20:18 +0000

    Funct- alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * main - prints the alphabet in lowercase
+ */
+void print_alphabet(void)
+
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
