0be1a51ea2480fa0c6ba553dbf6acb09df094cd4
Ana-Morales <moralesanaruth@gmail.com>
2020-06-18 19:30:38 +0000

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,43 @@
+/*#include "holberton.h"*/
+#include <stdio.h>
+/**
+ * fizzbuzz - prints the numbers from 1 to 100
+ *
+ * Description: Prints Fizz for mult of 3,
+ * Buzz for mult of 5, FizzBuzz for mult of both
+ */
+void fizzbuzz(void);
+int main(void)
+{
+	fizzbuzz();
+	return (0);
+}
+
+void fizzbuzz(void)
+{
+	int n;
+
+	n = 1;
+	while (n <= 100)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", n);
+		}
+		printf(" ");
+		n++;
+	}
+	printf("\n");
+}
