a3e4ca65b35f3f9e1315d395a61d483b52d6c730
Ana-Morales <moralesanaruth@gmail.com>
2020-06-18 15:53:12 +0000

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ */
+void more_numbers(void)
+{
+	int i, n;
+
+	i = 0;
+	while (i < 10)
+	{
+		n = 0;
+		while (n <= 14)
+		{
+			if (n > 9)
+			{
+				_putchar('0' + n / 10);
+			}
+			_putchar('0' + n % 10);
+			n++;
+		}
+		i++;
+		_putchar('\n');
+	}
+}
