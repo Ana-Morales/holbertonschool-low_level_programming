41649a978c31d510679313a69328c67e916915ea
Ana-Morales <moralesanaruth@gmail.com>
2020-06-18 16:01:51 +0000

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character _ should be printed
+ */
+void print_line(int n)
+{
+	int i;
+
+	i = 0;
+	while (n > 0 && i < n)
+	{
+		_putchar('_');
+		i++;
+	}
+	_putchar('\n');
+}
