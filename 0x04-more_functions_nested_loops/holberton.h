6e84412567833746e45c6d3f7b25cae475d91cc8
Ana-Morales <moralesanaruth@gmail.com>
2020-06-18 14:41:31 +0000

    Is uppercase

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,12 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+int _isupper(int c);
+#endif
