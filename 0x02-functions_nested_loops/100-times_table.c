c42f7a19cd43905239d67fce33009e819d3cc214
Ana-Morales <moralesanaruth@gmail.com>
2020-06-18 00:19:31 +0000

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,43 +1,70 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * print_times_table - prints the n times table
  */
 void print_times_table(int n)
 {
-	int f1, f2, m;
-	int m2;
+	int f1, f2, m, m2;
 
 	if (n >= 0 && n <= 15)
 	{
-		f1 = 0;
-		while (f1 <= n)
+		for (f1 = 0; f1 <= n; f1++)
 		{
-			f2 = 0;
-			while (f2 <= n)
+			for (f2 = 0; f2 <= n; f2++)
 			{
 				m = f1 * f2;
-				m2 = f1 * (f2 + 1);
-				printf("%d", m);
-				if (f2 == n)
+				if (f2 < n)
 				{
-					printf("\n");
-				}
-				else if (m > 99 || m2 > 99)
-				{
-					printf(", ");
-				}
-				else if (m > 9 || m2 > 9)
-				{
-					printf(",  ");
+					m2 = f1 * (f2 + 1);
+					if (m <= 9)
+					{
+						_putchar('0' + m);
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						if (m2 < 10)
+							_putchar(' ');
+					}
+					else if (m > 9 && m <= 99)
+					{
+						_putchar('0' + m / 10);
+						_putchar('0' + m % 10);
+						_putchar(',');
+						_putchar(' ');
+						if (m2 < 100)
+							_putchar(' ');
+					}
+					else if (m > 99)
+					{
+						_putchar('0' + m / 100);
+						_putchar('0' + (m - 100) / 10);
+						_putchar('0' + (m - 100) % 10);
+						_putchar(',');
+						_putchar(' ');
+					}
 				}
 				else
 				{
-					printf(",   ");
+					if (m <= 9)
+					{
+						_putchar('0' + m);
+						_putchar ('\n');
 					}
-				f2++;
+					else if (m > 9 && m <= 99)
+					{
+						_putchar('0' + m / 10);
+						_putchar('0' + m % 10);
+						_putchar('\n');
+					}
+					else if (m > 99)
+					{
+						_putchar('0' + m / 100);
+						_putchar('0' + (m - 100) / 10);
+						_putchar('0' + (m - 100) % 10);
+						_putchar('\n');
+					}
+				}
 			}
-			f1++;
 		}
 	}
 }
