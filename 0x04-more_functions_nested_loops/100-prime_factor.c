2afaf1c6ddb9cf16f7fa2be0418e3a9dc43847a3
Ana-Morales <moralesanaruth@gmail.com>
2020-06-19 22:18:37 +0000

    Prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+/**
+ * main - test fizzbuzz function
+ *
+ * is_prime - indentifies is a number is prime
+ * @n: number to test
+ *
+ * prime_factor: prints the largest prime factor of the number
+ * @n: number to find its largest prime factor
+ */
+int is_prime(long int n);
+void prime_factor(long int n);
+int main(void)
+{
+	long int i;
+
+	i = 612852475143;
+	prime_factor(i);
+	return (0);
+}
+
+int is_prime(long int n)
+{
+	long int d, p;
+
+	if (n == 1 || n == 2)
+	{
+		return (1);
+	}
+	else
+	{
+		d = 2;
+		while (n % d != 0 && d <= n)
+		{
+			d++;
+			p = d;
+		}
+		if (p == n)
+			return (1);
+		else
+			return (0);
+	}
+}
+
+void prime_factor(long int n)
+{
+	int p;
+	long int largest, d, n1;
+
+	p = is_prime(n);
+	n1 = n;
+
+	if (p == 1)
+	{
+		largest = p;
+	}
+	else
+	{
+		d = 2;
+		while (d <= n1)
+		{
+			while (n1 % d == 0)
+			{
+				largest = d;
+				n1 = n1 / d;
+			}
+			d++;
+		}
+	}
+	printf("%ld\n", largest);
+}
