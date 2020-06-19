ca7cad1e53a29ea6c78dfd883dab24a7f460f85b
Ana-Morales <moralesanaruth@gmail.com>
2020-06-19 00:16:34 +0000

    n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,70 +1,74 @@
 #include "holberton.h"
 /**
  * print_times_table - prints the n times table
+ * @n: n table to print
  */
+void times_table_line(int f, int n);
 void print_times_table(int n)
 {
-	int f1, f2, m, m2;
+	int f1;
 
 	if (n >= 0 && n <= 15)
 	{
 		for (f1 = 0; f1 <= n; f1++)
 		{
-			for (f2 = 0; f2 <= n; f2++)
+			times_table_line(f1, n);
+		}
+	}
+}
+/**
+ * times_table_line - prints (0 to f) x n line
+ * @f: First factor
+ * @n: second factor
+ */
+void times_table_line(int f, int n)
+{
+	int f2, m, m2;
+
+	for (f2 = 0; f2 <= n; f2++)
+	{
+		m = f * f2;
+		if (f2 < n)
+		{
+			m2 = f * (f2 + 1);
+			if (m > 99)
 			{
-				m = f1 * f2;
-				if (f2 < n)
-				{
-					m2 = f1 * (f2 + 1);
-					if (m <= 9)
-					{
-						_putchar('0' + m);
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						if (m2 < 10)
-							_putchar(' ');
-					}
-					else if (m > 9 && m <= 99)
-					{
-						_putchar('0' + m / 10);
-						_putchar('0' + m % 10);
-						_putchar(',');
-						_putchar(' ');
-						if (m2 < 100)
-							_putchar(' ');
-					}
-					else if (m > 99)
-					{
-						_putchar('0' + m / 100);
-						_putchar('0' + (m - 100) / 10);
-						_putchar('0' + (m - 100) % 10);
-						_putchar(',');
-						_putchar(' ');
-					}
-				}
-				else
-				{
-					if (m <= 9)
-					{
-						_putchar('0' + m);
-						_putchar ('\n');
-					}
-					else if (m > 9 && m <= 99)
-					{
-						_putchar('0' + m / 10);
-						_putchar('0' + m % 10);
-						_putchar('\n');
-					}
-					else if (m > 99)
-					{
-						_putchar('0' + m / 100);
-						_putchar('0' + (m - 100) / 10);
-						_putchar('0' + (m - 100) % 10);
-						_putchar('\n');
-					}
-				}
+				_putchar('0' + m / 100);
+				_putchar('0' + (m - 100) / 10);
+				_putchar('0' + (m - 100) % 10);
 			}
+			else if (m > 9)
+			{
+				_putchar('0' + m / 10);
+				_putchar('0' + m % 10);
+			}
+			else
+				_putchar('0' + m);
+			_putchar(',');
+			_putchar(' ');
+			if (m2 <= 99 && m <= 99)
+			{
+				_putchar(' ');
+				if (m2 < 10 && m <= 9)
+					_putchar(' ');
+			}
+		}
+		else
+		{
+			if (m > 99)
+			{
+				_putchar('0' + m / 100);
+				_putchar('0' + (m - 100) / 10);
+				_putchar('0' + (m - 100) % 10);
+			}
+			else if (m > 9)
+			{
+				_putchar('0' + m / 10);
+				_putchar('0' + m % 10);
+			}
+			else
+				_putchar('0' + m);
+			_putchar ('\n');
 		}
 	}
 }
