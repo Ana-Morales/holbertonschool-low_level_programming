dcc7f125ae2c4d050f0ece7fd329f60532f813ef
Ana-Morales <moralesanaruth@gmail.com>
2020-06-23 21:52:21 +0000

    with a temp char[509]

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,46 +2,27 @@
 /**
  * rev_string - reverses a string
  * @s: string to reverse
- * Return: the length of the string
  */
-int _strlen(char *s);
+
 void rev_string(char *s)
 
 {
-	int i, len, j;
-	char p[1000];
+	int i, len;
+	char p[509];
 
 
 	i = 0;
-	while (s[i] != '\0')
+	while( s[i] != '\0')
 	{
+
+		len = i + 1;
 		p[i] = s[i];
 		i++;
 	}
-
-	j = 0;
-	len = _strlen(s);
 	while (len > 0)
 	{
-		s[j] = p[len - 1];
+		*s = p[len -1];
+		s++;
 		len--;
-		j++;
 	}
-}
-/**
- * _strlen - returns the length of a string
- * @s: string to get its length
- * Return: the length of the string
- */
-int _strlen(char *s)
-{
-	int i, len;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		len = i + 1;
-		i++;
-	}
-	return (len);
 }
