f4558477b86a84275ccfb648edf611e62d70971f
Ana-Morales <moralesanaruth@gmail.com>
2020-06-23 16:46:21 +0000

    Print every other char

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,12 +5,11 @@
  */
 void puts2(char *str)
 {
-	int i;
 
-	while (str[i] != '\0')
+	while (*str != '\0')
 	{
-		_putchar(str[i]);
-		i = i + 2;
+		_putchar(*str);
+		str += 2;
 	}
 	_putchar('\n');
 }
