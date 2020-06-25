a0c4ec7c5ebab86fa0e43a6a31d6d7a4cd429945
Ana-Morales <moralesanaruth@gmail.com>
2020-06-25 23:41:26 +0000

    lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ *
+ *Return: string in uppercase
+ */
+char *string_toupper(char *s)
+{
+	char *out = s;
+
+	while ( *s != '\0')
+	{
+		if (*s >= 'a' && *s <= 'z')
+		{
+			*s = *s - 32;
+		}
+		s++;
+	}
+	return (out);
+}
