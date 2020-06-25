e0bcd6de0ae8de7a5e34a20f1c17bb96198ab447
Ana-Morales <moralesanaruth@gmail.com>
2020-06-25 21:43:20 +0000

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,38 +7,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int len_s1, len_s2;
-	char *s1_c = s1, *s2_c = s2;
-
-	len_s1 = 1;
-	len_s2 = 1;
-
-	while (*s1 != '\0')
+	while(*s1 == *s2)
 	{
-		++s1;
-		len_s1++;
+		s1++;
+		s2++;
+		if (*s1 == '\0')
+			return 0;
 	}
-	while (*s2 != '\0')
-	{
-		++s2;
-		len_s2++;
-	}
-	s1 = s1_c;
-	s2 = s2_c;
-	if (len_s1 > len_s2)
-	{
-		if ((*s1 - *s2) < 0)
-			return (-1 * (*s1 - *s2));
-		else
-			return (*s1 - *s2);
-	}
-	else if (len_s1 < len_s2)
-	{
-		if ((*s1 - *s2) < 0)
-			return (*s1 - *s2);
-		else
-			return (-1 * (*s1 - *s2));
-	}
-	else
-		return (0);
+	return *s1 - *s2;
 }
