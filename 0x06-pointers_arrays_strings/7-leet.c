225463c386ebbec386e0ae534d353f7759a6783e
Ana-Morales <moralesanaruth@gmail.com>
2020-06-26 02:11:25 +0000

    encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * leet - encodes a string into 1337.
+ *
+ * @s: string
+ *
+ */
+char *leet(char *s)
+{
+	char c[] = "a, A, e, E, o, O, t, T, l, L";
+	char t[] = "4, 4, 3, 3, 0, 0, 7, 7, 1, 1";
+	int i;
+	char *p = s;
+
+	while (*s != '\0')
+	{
+		i = 0;
+		while (c[i] != '\0')
+		{
+			if (*s == c[i])
+				*s = t[i];
+			i++;
+		}
+		s++;
+	}
+	return (p);
+}
