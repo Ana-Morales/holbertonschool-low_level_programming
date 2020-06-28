40a8faa5adce1b0cdf7b41b8e13564fe12b5730c
Ana-Morales <moralesanaruth@gmail.com>
2020-06-28 14:37:06 +0000

    locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,10 +7,11 @@
  */
 char *_strchr(char *s, char c)
 {
+	int i = 0;
 
-	while (*s != '\0' && *s != c)
+	while (s[i] != '\0' && s[i] != c)
 	{
-		s++;
+		i++;
 	}
-	return (s);
+	return (s + i);
 }
