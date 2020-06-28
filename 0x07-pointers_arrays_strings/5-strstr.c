c21aa9a5afb5d0597544e924d70c7219f7ff3010
Ana-Morales <moralesanaruth@gmail.com>
2020-06-28 16:57:17 +0000

    fix for string of 1 char

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -19,13 +19,13 @@ char *_strstr(char *haystack, char *needle)
 		j = 0;
 		while (needle[j] != '\0' && s[i] == needle[j])
 		{
+			if (needle[j + 1] == '\0')
+				return (s);
 			if (s[i + 1] == needle[j + 1])
 			{
 				j++;
 				i++;
 			}
-			else if (needle[j + 1] == '\0')
-				return (s);
 			else
 				break;
 		}
