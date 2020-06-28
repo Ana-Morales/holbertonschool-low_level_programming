31708de0008fe07997354099e3995de94c24e8c7
Ana-Morales <moralesanaruth@gmail.com>
2020-06-28 02:24:59 +0000

    searches a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * __strpbrk - searches a string for any of a set of bytes
+ *
+ * @s: string to be scanned
+ * @accept: string containing the characters to match
+ * Return: pointer to the byte in s that matches one of the bytes in accept
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	char *p = 0;
+
+	while (*s != '\0')
+	{
+		i = 0;
+		while (accept[i] != '\0')
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+			i++;
+		}
+		s++;
+	}
+	return (p);
+}
