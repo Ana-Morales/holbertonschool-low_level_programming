d172ee2d13ddaa0f0b38e61950894eab83dde110
Ana-Morales <moralesanaruth@gmail.com>
2020-07-03 23:45:49 +0000

    Fixed for Betty style

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,16 +2,14 @@
 #include <stdlib.h>
 /**
  * main -  prints the min number of coins to make change for an amount of money
- * @argc: argument count
- * @argv: argument values
+ * @money: amount of money to change
  * Return: 0 is success, 1 if error.
  */
-
+int coins(int money);
 int main(int argc, char **argv)
 {
-	int money, coins;
+	int money, total;
 
-	coins = 0;
 	if (argc != 2)
 	{
 		printf("Error\n");
@@ -20,65 +18,56 @@ int main(int argc, char **argv)
 	money = atoi(argv[1]);
 	if (money <= 0)
 	{
-		printf("0");
+		printf("0\n");
 		return (0);
 	}
-	else if (money / 25 > 0)
+	total = coins(money);
+	printf("%d\n", total);
+	return (0);
+}
+/**
+ * coins - count the number of coins needed
+ * @money: amount of money to change
+ * Return: number of coins
+ */
+int coins(int money)
+{
+	int coins;
+
+	coins = 0;
+
+	if (money / 25 > 0)
 	{
-		coins = money / 25;
+		coins = coins + money / 25;
 		if (money % 25 == 0)
-		{
-			printf("%d\n", coins);
-			return (0);
-		}
-		else
-			money = money % 25;
+			return (coins);
+		money = money % 25;
 	}
 	if (money / 10 > 0)
 	{
-		coins = coins + money / 10;
+		coins = coins + (money / 10);
 		if (money % 10 == 0)
-		{
-			printf("%d\n", coins);
-			return (0);
-		}
-		else
-			money = money % 10;
+			return (coins);
+		money = money % 10;
 	}
 	if (money / 5 > 0)
 	{
 		coins = coins + money / 5;
 		if (money % 5 == 0)
-		{
-			printf("%d\n", coins);
-			return (0);
-		}
-		else
-			money = money % 5;
+			return (coins);
+		money = money % 5;
 	}
 	if (money / 2 > 0)
 	{
 		coins = coins + money / 2;
 		if (money % 2 == 0)
-		{
-			printf("%d\n", coins);
-			return (0);
-		}
-		else
-		{
-			money = money % 2;
-		}
+			return (coins);
+		money = money % 2;
 	}
 	if (money / 1 > 0)
 	{
-		coins = coins + money / 1;
-		if (money % 1 == 0)
-		{
-			printf("%d\n", coins);
-			return (0);
-		}
-		else
-			money = money % 1;
+		coins = coins + money;
+			return (coins);
 	}
 	return (0);
 }
