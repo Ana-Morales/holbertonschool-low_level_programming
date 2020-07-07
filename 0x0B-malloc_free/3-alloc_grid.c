6c4c0152c4e99b0bbf6593af0f3068213f8198e4
Ana-Morales <moralesanaruth@gmail.com>
2020-07-07 23:22:12 +0000

    Fixed for fail

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 {
 	int **g, i, j;
 
-	if (width == 0 || height == 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 	g = (int **)malloc(sizeof(int *) * height);
 	if (g == NULL)
@@ -23,7 +23,10 @@ int **alloc_grid(int width, int height)
 	{
 		g[i] = (int *)malloc(sizeof(int) * width);
 		if (g[i] == NULL)
+		{
+			free(g);
 			return (NULL);
+		}
 		i++;
 	}
 	i = 0;
