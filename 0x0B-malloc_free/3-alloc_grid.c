51a0b08d50df5b8cecb3514a128a1cf245808c32
Ana-Morales <moralesanaruth@gmail.com>
2020-07-07 22:45:01 +0000

    Testing free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 {
 	int **g, i, j;
 
-	if (width <= 0 || height <= 0)
+	if (width == 0 || height == 0)
 		return (NULL);
 	g = (int **)malloc(sizeof(int *) * height);
 	if (g == NULL)
