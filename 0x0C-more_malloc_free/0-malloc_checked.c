f6b733ac2d64ce35e981fe2937fc5360d7ec9df0
Ana-Morales <moralesanaruth@gmail.com>
2020-07-09 16:39:54 +0000

    allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: amount of memory required
+ *
+ * Return: pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		free(p);
+		exit(98);
+	}
+	return (p);
+	free(p);
+}
