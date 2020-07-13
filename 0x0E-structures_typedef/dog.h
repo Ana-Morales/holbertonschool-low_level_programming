8956ddd465efaec2cdbb35884d952cc5592bbf9f
Ana-Morales <moralesanaruth@gmail.com>
2020-07-13 20:38:30 +0000

    Header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - contain the name, age and owner of dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c);
+
+#endif /*_DOG_H*/
