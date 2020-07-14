f6ce9c4466ed2d2bfe1384c58fc14d027d911327
Ana-Morales <moralesanaruth@gmail.com>
2020-07-14 01:08:16 +0000

    bugs fixed

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -39,26 +39,31 @@ dog_t *new_dog(char *name, float age, char *owner)
 	len_n = _strlen(name);
 	p->name = malloc(sizeof(char) * (len_n + 1));
 	if (p->name == NULL)
+	{
+		free(p);
 		return (NULL);
+	}
 	i = 0;
-	while (i < len_n)
+	while (i <= len_n)
 	{
 		p->name[i] = name[i];
 		i++;
 	}
-	p->name[i] = '\0';
+	p->age = age;
 	i = 0;
 	len_o = _strlen(owner);
 	p->owner = malloc(sizeof(char) * (len_o + 1));
 	if (p->owner == NULL)
+	{
+		free(p->name);
+		free(p);
 		return (NULL);
+	}
 	i = 0;
-	while (i < len_o)
+	while (i <= len_o)
 	{
 		p->owner[i] = owner[i];
 		i++;
 	}
-	p->owner[i] = '\0';
-	p->age = age;
 	return (p);
 }
