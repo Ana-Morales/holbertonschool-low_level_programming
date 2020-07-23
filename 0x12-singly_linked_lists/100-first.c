f5875bb90a1b6fd4c5f0ae656e01bf21783fc517
Ana-Morales <moralesanaruth@gmail.com>
2020-07-23 21:39:06 +0000

    Create a function to be execute before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * before_main - function to be execute before main function
+ *
+ */
+void before_main(void) __attribute__ ((constructor));
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
