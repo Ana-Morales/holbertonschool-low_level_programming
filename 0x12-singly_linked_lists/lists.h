1c543a1f412b519ad40ff3f166dd3e1302ef42be
Ana-Morales <moralesanaruth@gmail.com>
2020-07-23 17:13:26 +0000

    Add libraries

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
-#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
