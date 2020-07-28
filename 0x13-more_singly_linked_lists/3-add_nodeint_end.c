11cc667c11e26784408d95a024bd967250304311
Ana-Morales <moralesanaruth@gmail.com>
2020-07-28 12:36:44 +0000

    Add return in if statement

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -18,7 +18,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new->n = n;
 	new->next = NULL;
 	if (*head == NULL)
+	{
 		*head = new;
+		return (*head);
+	}
 	else
 	{
 		while (last->next != NULL)
