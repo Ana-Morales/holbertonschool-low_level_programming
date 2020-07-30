71190825e59eca0fc68b511541f673aa0dd8cec7
Ana-Morales <moralesanaruth@gmail.com>
2020-07-30 22:55:49 +0000

    Define get_endianness function

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * get_endianness -  checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c;
+
+	c = (char *)&i;
+	if (*c != '\0')
+		return (1);
+	else
+		return (0);
+}
