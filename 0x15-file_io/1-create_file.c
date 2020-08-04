491d7b848450d218bb49924f8037fdeb2eeedbc2
Ana-Morales <moralesanaruth@gmail.com>
2020-08-04 20:46:45 +0000

    Add condition for text_content == NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -35,9 +35,14 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd != -1)
 	{
-		n = write(fd, text_content, _strlen(text_content));
+		if (text_content == NULL)
+			n = write(fd, "", 0);
+		else
+			n = write(fd, text_content, _strlen(text_content));
 		if (n == -1)
+		{
 			return (-1);
+		}
 		close(fd);
 		return (1);
 	}
