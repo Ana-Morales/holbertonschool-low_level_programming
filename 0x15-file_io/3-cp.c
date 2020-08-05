61df1ab4d7e91817b9b798f3bc459de7e63157b1
Ana-Morales <moralesanaruth@gmail.com>
2020-08-05 00:14:28 +0000

    Fix for Betty style

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,16 +19,16 @@ int _cp(const char *filename_from, const char *filename_to)
 
 	fd_from = open(filename_from, O_RDONLY);
 	if (fd_from == -1)
-		dprintf(2, "Error: Can't read from file %s\n", filename_from), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename_from), exit(98);
 	fd_to = open(filename_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
-		dprintf(2, "Error: Can't write to %s\n", filename_to), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename_to), exit(99);
 	while (1)
 	{
 		n = read(fd_from, buffer, 1024);
 		if (n == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", filename_from);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename_from);
 			exit(98);
 		}
 		if (n < 1024)
@@ -36,21 +36,21 @@ int _cp(const char *filename_from, const char *filename_to)
 			n = write(fd_to, buffer, n);
 			if (n == -1)
 			{
-				dprintf(2, "Error: Can't write to %s\n", filename_to);
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename_to);
 				exit(99);
 			}
 			break;
 		}
 		n = write(fd_to, buffer, n);
 		if (n == -1)
-			dprintf(2, "Error: Can't write to %s\n", filename_to), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename_to), exit(99);
 	}
 	n = close(fd_from);
 	if (n == -1)
-		dprintf(2, "Error: Can't close fd %d\n", fd_from), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
 	n = close(fd_to);
 	if (n == -1)
-		dprintf(2, "Error: Can't close fd %d\n", fd_to), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 	return (1);
 }
 /**
@@ -64,7 +64,7 @@ int main(int argc, char **argv)
 {
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	_cp(argv[1], argv[2]);
