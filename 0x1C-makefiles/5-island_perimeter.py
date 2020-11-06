953e0c0523ec422225ab5b1db1651881f23ea5d5
Ana-Morales <moralesanaruth@gmail.com>
2020-11-06 19:36:29 +0000

    First commit

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Island_perimeter module"""
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid"""
+    x_min = 0
+    x_max = 0
+    y_min = 0
+    y_max = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if x_min == 0 or i < x_min:
+                    x_min = i
+                if i > x_max:
+                    x_max = i
+                if y_min == 0 or j < y_min:
+                    y_min = j
+                if j > y_max:
+                    y_max = j
+    return (x_max - x_min + 1)*2 + (y_max - y_min + 1)*2
