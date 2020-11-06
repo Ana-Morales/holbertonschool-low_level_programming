9117b73dd5c9e71849ceb2890057dc35b43133e8
Ana-Morales <moralesanaruth@gmail.com>
2020-11-06 21:13:49 +0000

    Fix for taking the whole grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,15 +5,17 @@
 def island_perimeter(grid):
     """Returns the perimeter of the island described in grid"""
     p = 0
-    for i in range(1, (len(grid) - 1)):
-        for j in range(1, (len(grid[i])-1)):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if grid[i-1][j] == 0:
+                if i == 0 or (i != 0 and grid[i-1][j] == 0):
                     p += 1
-                if grid[i+1][j] == 0:
+                if i == len(grid) - 1 or (i != (len(grid) - 1) and
+                                          grid[i+1][j] == 0):
                     p += 1
-                if grid[i][j-1] == 0:
+                if j == 0 or (j != 0 and grid[i][j-1] == 0):
                     p += 1
-                if grid[i][j+1] == 0:
+                if j == len(grid[i]) - 1 or (j != (len(grid[i]) - 1) and
+                                             grid[i][j+1] == 0):
                     p += 1
     return p
